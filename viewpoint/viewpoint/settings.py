04bfd98ee317452d2e472518f99f4a1204ecc4ce
darraghmurphy1 <darraghmurphy77@gmail.com>
2022-11-15 15:10:46 +0000

    crispy forms installed and added to installed apps

--- viewpoint/viewpoint/settings.py
+++ viewpoint/viewpoint/settings.py
@@ -31,13 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'feed.apps.FeedConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'viewpoint',
 ]
 
 MIDDLEWARE = [
