6dddf072650c9e2f367a2f1d1a7d907f60789e4d
darraghmurphy1 <darraghmurphy77@gmail.com>
2022-11-15 15:08:53 +0000

    feed and users apps created

--- viewpoint/viewpoint/settings.py
+++ viewpoint/viewpoint/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'viewpoint',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'viewpoint.urls'
+ROOT_URLCONF = 'viewpoint.viewpoint.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'viewpoint.wsgi.application'
+WSGI_APPLICATION = 'viewpoint.viewpoint.wsgi.application'
 
 
 # Database
--- viewpoint/viewpoint/wsgi.py
+++ viewpoint/viewpoint/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewpoint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'viewpoint.viewpoint.settings')
 
 application = get_wsgi_application()
